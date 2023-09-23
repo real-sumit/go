8efce19d12d6bccb097377b61ec15247a57683ca
Sumit Modak <142516692+real-sm@users.noreply.github.com>
2023-09-23 10:08:47 +0530

    changed structs

--- structs.go
+++ structs.go
@@ -1,9 +1,87 @@
 package main
 import "fmt"
 
+type user struct {
+	name string
+	phone int
+}
+
+// nested struct
+type msg struct {
+	message string
+	sender user
+	recipient user
+}
+
 type car struct {
-	Make string
-	Model string
-	height int 
-	width int
+	make string
+	model string
+	// anonymous struct
+	Wheel struct {
+		radius int 
+		material string
+	}
+}
+
+// embedded struct
+type ownedCar struct {
+	user
+	make string
+	model string
+}
+
+type cuboid struct {
+	length int 
+	width int 
+	height int
+}
+
+// take struct as self
+func (c cuboid) volume() int {
+	return c.length * c.width * c.height
+}
+
+func main() {
+	m := user {
+		phone: 148255510961,
+		name: "Sumit",
+	}
+	fmt.Println(m)
+
+	m2 := msg {}
+	m2.message = "Thanks for signing up"
+	m2.sender.phone = 1234567890
+	m2.sender.name = "Boot dev"
+	m2.recipient.phone = 148255510961
+	m2.recipient.name = "Sumit"
+	fmt.Println(m2)
+
+	// anonymous structs
+	myCar := struct {
+		make string
+		model string
+	} {
+		make: "tesla",
+		model: "model 3",
+	}
+	fmt.Println(myCar)
+
+	// embedded structs
+	myCar2 := ownedCar {
+		make: "bugatti",
+		model: "chiron",
+		user: user {
+			name: "Sumit",
+			phone: 148255510961,
+		},
+	}
+	fmt.Println(myCar2)
+
+	// struct methods
+	c := cuboid {
+		length: 7,
+		width: 5,
+		height: 16,
+	}
+	fmt.Println(c.volume())
 }
